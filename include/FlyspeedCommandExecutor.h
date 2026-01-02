0274155eb45d13cff7114765d62677be85bd3dcf
Nipitare <nipitareyt@gmail.com>
2026-01-02 11:39:21 +0200

    Update /flyspeed command
    Changes:
    - Inputting negative multipliers will now default the speed to 1
    - Shows your current flight speed multiplier if multiplier is unspecified

--- include/FlyspeedCommandExecutor.h
+++ include/FlyspeedCommandExecutor.h
@@ -8,11 +8,13 @@ class FlySpeedCommandExecutor : public endstone::CommandExecutor {
 public:
     bool onCommand(endstone::CommandSender &sender, const endstone::Command &command,
              const std::vector<std::string> &args) override {
-        float flightSpeedMultiplier = 1;
-        if (!args.empty())
-            flightSpeedMultiplier = std::stof(args[0]);
-        sender.sendMessage("Flight speed multiplier set to " + fmt::format("{}", flightSpeedMultiplier) + "x");
-        sender.asPlayer()->setFlySpeed(flightSpeedMultiplier * VANILLA_FLY_SPEED);
+        float flightSpeedMultiplier = sender.asPlayer()->getFlySpeed() * 20;
+        if (!args.empty()) {
+            flightSpeedMultiplier = std::stof(args[0]) >= 0 ? std::stof(args[0]) : 1;
+            sender.sendMessage("Flight speed multiplier set to " + fmt::format("{}", flightSpeedMultiplier) + "x");
+            sender.asPlayer()->setFlySpeed(flightSpeedMultiplier * VANILLA_FLY_SPEED);
+        }
+        else sender.sendMessage("Current flight speed multiplier is " + fmt::format("{}", flightSpeedMultiplier)+ "x");
         return true;
     }
 };
