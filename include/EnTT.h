45d24a1ebfec5e1146d98f25665f94480d64f087
Nipitare <nipitareyt@gmail.com>
2026-04-04 20:34:58 +0300

    Setup EnTT and make project c++ 23
    EnTT was added for getUniqueIDFromActor

---
+++ include/EnTT.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <entt/entt.hpp>
+
+struct EntityId {
+    std::uint32_t rawId;
+
+    [[nodiscard]] constexpr bool operator==(const EntityId& other) const = default;
+
+    [[nodiscard]] constexpr operator std::uint32_t() const {
+        return this->rawId;
+    }
+};
+
+struct EntityIdTraits {
+    using value_type = EntityId;
+
+    using entity_type = std::uint32_t;
+    using version_type = std::uint16_t;
+
+    static constexpr entity_type entity_mask = 0x3FFFF; // lower 18 bits of raw id
+    static constexpr entity_type version_mask = 0x3FFF; // upper 14 bits of raw id
+};
+template<>
+struct entt::entt_traits<EntityId> : entt::basic_entt_traits<EntityIdTraits> {
+    static constexpr std::size_t page_size = 2048;
+};
+
+struct IEntityComponent {};
+
+template<std::derived_from<IEntityComponent> Type>
+struct entt::component_traits<Type, EntityId> {
+    using element_type = Type;
+    using entity_type = EntityId;
+    static constexpr bool in_place_delete = true;
+    static constexpr std::size_t page_size = 128 * !std::is_empty_v<Type>;
+};
+
+template<typename Type>
+struct entt::storage_type<Type, EntityId> {
+    using type = basic_storage<Type, EntityId>;
+};
+
+struct ActorUniqueIDComponent : IEntityComponent {
+    int64_t mActorUniqueID;
+};
+
+struct EntityRegistry : std::enable_shared_from_this<EntityRegistry> {
+    std::string name;
+    entt::basic_registry<EntityId> registry;
+    uint32_t id;
+};
+
+struct EntityContext {
+    EntityRegistry& registry;
+    entt::basic_registry<EntityId>& enttRegistry;
+    EntityId entity;
+
+    template<std::derived_from<IEntityComponent> T>
+    [[nodiscard]] T* tryGetComponent() {
+        return this->enttRegistry.try_get<T>(this->entity);
+    }
+};
+
+class Actor {
+public:
+    [[nodiscard]] auto& getEntity(this auto& self) {
+        return *reinterpret_cast<EntityContext*>(reinterpret_cast<std::uintptr_t>(&self) + 0x8);;
+    }
+};
--- include/Utils.h
+++ include/Utils.h
@@ -1,21 +1,11 @@
 #pragma once
 
-#include <cstdint>
-#include "hook.h"
-
-extern void* getBaseAddress();
+#include <EnTT.h>
 
 class Utils {
 public:
     static int64_t getUniqueIDFromActorPtr(void *actor) {
-        using GetActorUniqueIDComponentFn = int64_t *(__fastcall*)(void *actor);
-        void *baseAddress = getBaseAddress();
-        GetActorUniqueIDComponentFn getActorUniqueIDComponent =
-            (GetActorUniqueIDComponentFn)((char*)baseAddress + 59202864); // address of "Actor::getOrCreateUniqueID"
-        int64_t *actorUniqueIDComponentPtr = getActorUniqueIDComponent(actor);
-        if (actorUniqueIDComponentPtr) {
-            return *actorUniqueIDComponentPtr;
-        }
-        return -1;
+        EntityContext player = reinterpret_cast<Actor*>(actor)->getEntity();
+        return player.tryGetComponent<ActorUniqueIDComponent>()->mActorUniqueID;
     }
 };
